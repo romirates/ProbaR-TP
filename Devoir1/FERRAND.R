fe0c1da91c1f11a0cdbe7848bbe0c89fbf0d973a
Romain Ferrand <romain.ferrand@gmx.fr>
2017-03-25 10:20:10 +0100

    Ajout devoir1

--- Devoir1/FERRAND.R
+++ Devoir1/FERRAND.R
@@ -26,48 +26,53 @@ simule = function(N, n)
   }
 ######################
 ########################
+set.seed(7486)#initialisation de la graine pour tout les tests
 N = 1000 # nombre de testes
-set.seed(7486) # remplacer 1374 par 
-# les 4 derniers chiffres de votre code ETUDIANT,
-# en enlevant \'eventuellement z\'ero quand il est en premi\`ere position)
 nV = 10:99
 L = length(nV)
 xV =0*nV # Initialisation
 yV = 0*nV # Initialisation
-# A completer
+
 #test pour N = 1000
+
 #Changer le xV avec for (i in 1 :L) pour que xV[i] soit simule(N, nV[i])
 for(i in 1:L){xV[i] = simule(N,nV[i])}
+
 #Changer yV avec for (i in 1 :L) pour que yV[i] soit la probabilité exacte p(nV[i])
 for(i in 1:L){yV[i] = p(nV[i])}
+
 #Faire un plot de (nV,xV) en bleu et de (nV,yV) en rouge dans la même figure.
 plot(nV,xV, xlab = "n : cardinal de l'ensemble", ylab = "Probabilité de 6 val. distinctes parmi n (rouge : proba théo./bleu : proba exp.)", col="blue")
 points(nV,yV, col="red")
-#les proba sont proche 
+
 # Calculer la moyenne m de xV - yV. Dans R fonction moyenne est mean()
 m<-mean(xV-yV)
 #proche de 0 cela veut dire que l'expérience est proche de la théorie
+
 #Calculer la moyenne de (xV − yV − m)^2 , (c’est la variance de xV-yV).
 varianceXY<-mean((xV-yV-m)^2)
+
 #répété ces opération avec N = 2000
   N = 2000 # nombre de testes
   set.seed(7486) # remplacer 1374 par 
-  # les 4 derniers chiffres de votre code ETUDIANT,
-  # en enlevant \'eventuellement z\'ero quand il est en premi\`ere position)
   nV = 10:99
   L = length(nV)
   xV =0*nV # Initialisation
   yV = 0*nV # Initialisation
+  
   #Changer le xV avec for (i in 1 :L) pour que xV[i] soit simule(N, nV[i])
   for(i in 1:L){xV[i] = simule(N,nV[i])}
+  
   #Changer yV avec for (i in 1 :L) pour que yV[i] soit la probabilité exacte p(nV[i])
   for(i in 1:L){yV[i] = p(nV[i])}
+  
   #Faire un plot de (nV,xV) en bleu et de (nV,yV) en rouge dans la même figure.
   plot(nV,xV, xlab = "n : cardinal de l'ensemble", ylab = "Probabilité de 6 val. distinctes parmi n (rouge : proba théo./bleu : proba exp.)", col="blue")
   points(nV,yV, col="red")
   
   # Calculer la moyenne m de xV - yV. Dans R fonction moyenne est mean()
   m<-mean(xV-yV)
+  
   #Calculer la moyenne de (xV − yV − m)^2 , (c’est la variance de xV-yV).
   varianceXY<-mean((xV-yV-m)^2)
   
@@ -80,9 +85,9 @@ varianceXY<-mean((xV-yV-m)^2)
 f = function(m, n)
 { x = 1:n
    v = sample(x, m, replace=FALSE)
-   #teste = ( v == sort(v)) # sort(v) ordonne v
+   teste = ( v == sort(v)) # sort(v) ordonne v
    z = 0
-   if ( v == sort(v) ) { z = 1 }
+   if ( m == sum(teste)) { z = 1}
    return( z )
    }
 #############
@@ -99,34 +104,42 @@ simuleV = function(N, m, nV)
   }
 #############
 ########################
+  
 mV <- 2:8
 pV <- 0*mV
 l<- length(mV)
 nV <- 10:30
 N <- 500 # nombre de tests
-set.seed(7486)
+
 # Créer un vecteur pV de même taille que mV tel que
 # pV[i] = mean( simuleV(N, mV[i], nV) ).
 for(i in 1:l){pV[i] <- mean(simuleV(N, mV[i], nV))}
+
 # Creer un vecteur P de meme taille que que mV tel que 
-# P[i] = 1/mV[i] ! : c’est la probabilité exacte.
-P<-1/mV
+# P[i] = 1/mV[i]! : c’est la probabilité exacte.
+P<-1/factorial(mV)
+
 #Calculer le maximum des erreurs en valeurs absolues max( abs(pV - P)))
-maxErreur<-max(abs(pV-p))
+maxErreur<-max(abs(pV-P))
+
 # Tracer en bleu les points(mV[i], pV[i]) et y ajouter en rouge la courbe (x,y)=(mV, P).
 plot(mV, pV, xlab = "Taille de la suite d'entier ordonée", ylab ="Probabilité de la suite (rouge : proba exp. /bleu proba théo.)", col="red")
 points(mV,P, col="blue")
+
 #Calculer la moyenne des erreurs |pV[i] -P[i]|
-moyErreur<-mean(abs(pV-p))
+moyErreur<-mean(abs(pV-P))
 
 #Pour N = 1500
 N<-1500
 for(i in 1:l){pV[i] <- mean(simuleV(N, mV[i], nV))}
-#Calculer le maximum des erreurs en valeurs absolues max( abs(pV - P)))
+
+#Calculer le maximum de erreurs en valeurs absolues max(|pV - P|)
 maxErreur<-max(abs(pV-p))
+
 # Tracer en bleu les points(mV[i], pV[i]) et y ajouter en rouge la courbe (x,y)=(mV, P).
 plot(mV, pV, xlab = "Taille de la suite d'entier ordonée", ylab ="Probabilité de la suite (rouge : proba exp. /bleu proba théo.)", col="red")
 points(mV,P, col="blue")
+
 #Calculer la moyenne des erreurs |pV[i] -P[i]|
 moyErreur<-mean(abs(pV-p))
 
